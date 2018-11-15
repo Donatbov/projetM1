8b85af50e34b48d450c7467776a58a8f2bc0d8a4
zhanglei <1280368848@qq.com>
2018-11-15 09:05:39 +0100

    modification de la class graphe

--- Graphe.py
+++ Graphe.py
@@ -6,9 +6,31 @@ class Graphe(object):
 
     def create(self, type_graphe):
         if type_graphe == 0:
-            return Largage()
+            return LargageHR()
         elif type_graphe == 1:
-            return Triangle()
+            return LargageHE()
+        elif type_graphe == 2:
+            return LargageAR()
+        elif type_graphe == 3:
+            return LargageAE()
+        elif type_graphe == 4:
+            return Attaque()
+        elif type_graphe == 5:
+            return Envisage()
+        elif type_graphe == 6:
+            return Appui()
+        elif type_graphe == 7:
+            return AttaqueJE()
+        elif type_graphe == 8:
+            return AttaqueJ()
+        elif type_graphe == 9:
+            return AttaquePFE()
+        elif type_graphe == 10:
+            return AttaquePF()
+        elif type_graphe == 11:
+            return PEPS()
+        elif type_graphe == 12:
+            return PPS()
         else:
             return -1
 
