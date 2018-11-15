58beb1cb825dfc51856d764b6d017923e0ae9729
zhanglei <1280368848@qq.com>
2018-11-15 09:08:13 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,9 +19,6 @@ class Example(QWidget):
         self.graphe = Graphe()
         self.type_graphe = 0
 
-        self.largageButton          = QPushButton('Largage', self)
-        self.lineTriangleButton     = QPushButton('LineTriangle', self)
-
         
         self.initUI()
 
@@ -33,14 +30,90 @@ class Example(QWidget):
         self.setGeometry(200, 100, 1000, 600)  # taille par défautt de la fenetre
         self.setWindowTitle('Santoline')
 
-        grid.addWidget(self.largageButton, 0, 1, Qt.AlignBottom)
-        grid.addWidget(self.lineTriangleButton, 0, 4, Qt.AlignBottom)
+        self.choixLargage = QComboBox()
+        self.choixLargage.addItems(["Largage HBE au retardant","Largage HBE en eau", "Largage ABE au retardant", "Largage HBE en eau"])
+        self.attaqueButton = QPushButton('Attaque', self)
+        self.envisageButton = QPushButton('appui envisagee', self)
+        self.appuiButton = QPushButton('appui', self)
+        self.attaqueJEButton = QPushButton('attaqueJE', self)
+        self.attaqueJButton = QPushButton('attaqueJ', self)
+        self.attaquePFEButton = QPushButton('attaquePFE', self)
+        self.attaquePFButton = QPushButton('attaquePF', self)
+        self.PEPSButton = QPushButton('PEPS', self)
+        self.PPSButton = QPushButton('PPS', self)
 
-        self.largageButton.clicked[bool].connect(self.set_type_largage)
-        self.lineTriangleButton.clicked[bool].connect(self.set_type_line_triangle)
+
+        grid.addWidget(self.choixLargage, 0, 0, Qt.AlignBottom)
+        grid.addWidget(self.attaqueButton, 0, 1, Qt.AlignBottom)
+        grid.addWidget(self.envisageButton, 0, 2, Qt.AlignBottom)
+        grid.addWidget(self.appuiButton, 0, 3, Qt.AlignBottom)
+        grid.addWidget(self.attaqueJEButton, 0, 4, Qt.AlignBottom)
+        grid.addWidget(self.attaqueJButton, 0, 5, Qt.AlignBottom)
+        grid.addWidget(self.attaquePFEButton, 0, 6, Qt.AlignBottom)
+        grid.addWidget(self.attaquePFButton, 0, 7, Qt.AlignBottom)
+        grid.addWidget(self.PEPSButton, 0, 8, Qt.AlignBottom)
+        grid.addWidget(self.PPSButton, 0, 9, Qt.AlignBottom)
+
+        self.choixLargage.currentIndexChanged.connect(self.selectionchangeLargage)
+        self.attaqueButton.clicked[bool].connect(self.set_type_attaque)
+        self.envisageButton.clicked[bool].connect(self.set_type_envisage)
+        self.appuiButton.clicked[bool].connect(self.set_type_appui)
+        self.attaqueJEButton.clicked[bool].connect(self.set_type_attaqueJE)
+        self.attaqueJButton.clicked[bool].connect(self.set_type_attaqueJ)
+        self.attaquePFEButton.clicked[bool].connect(self.set_type_attaquePFE)
+        self.attaquePFButton.clicked[bool].connect(self.set_type_attaquePF)
+        self.PEPSButton.clicked[bool].connect(self.set_type_PEPS)
+        self.PPSButton.clicked[bool].connect(self.set_type_PPS)
 
         self.show()
 
+    def selectionchangeLargage(self, i):
+        if i == 0:
+            self.type_graphe = 0
+        elif i == 1:
+            self.type_graphe = 1
+        elif i == 2:
+            self.type_graphe = 2
+        else:
+            self.type_graphe = 3
+        self.update()
+
+    def set_type_attaque(self, x):
+        self.type_graphe = 4
+        self.isNewGraph = False
+
+    def set_type_envisage(self, x):
+        self.type_graphe = 5
+        self.isNewGraph = False
+
+    def set_type_appui(self, x):
+        self.type_graphe = 6
+        self.isNewGraph = False
+
+    def set_type_attaqueJE(self, x):
+        self.type_graphe = 7
+        self.isNewGraph = False
+    
+    def set_type_attaqueJ(self, x):
+        self.type_graphe = 8
+        self.isNewGraph = False
+
+    def set_type_attaquePFE(self, x):
+        self.type_graphe = 9
+        self.isNewGraph = False
+
+    def set_type_attaquePF(self, x):
+        self.type_graphe = 10
+        self.isNewGraph = False
+
+    def set_type_PEPS(self, x):
+        self.type_graphe = 11
+        self.isNewGraph = False
+    
+    def set_type_PPS(self, x):
+        self.type_graphe = 12
+        self.isNewGraph = False
+        
     # fonction qui réagit à l'évènement : la souris bouge
     def mouseMoveEvent(self, e):
         self.pos = e.pos()
